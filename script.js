ddf128693506678faa1b3040d7d6d02d05033e24
w0rkee <w0rkee.info@gmail.com>
2026-04-08 04:30:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+/* FUNZIONE PER INIZIALIZZARE GOOGLE TRANSLATE */
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'it', // Lingua originale del tuo sito
+        includedLanguages: 'en,fr,es,de', // Lingue in cui vuoi tradurre
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+}
 
+/* CARICAMENTO DELLO SCRIPT DI GOOGLE */
+(function() {
+    var gt = document.createElement('script');
+    gt.type = 'text/javascript';
+    gt.async = true;
+    gt.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gt, s);
+})();
